6156a3e81344b1bb2b617d653a1c86615202befa
Nanaya's Workshop <154449746+NanayasWorkshop@users.noreply.github.com>
2025-05-06 15:47:51 +0200

    FixJob

--- workshop_app/templatetags/workshop_extras.py
+++ workshop_app/templatetags/workshop_extras.py
@@ -5,4 +5,12 @@ register = template.Library()
 @register.filter
 def getattr(obj, attr):
     """Gets an attribute of an object dynamically from a string name"""
-    return obj.__getattribute__(attr)
+    try:
+        # First try to get the attribute directly
+        return obj.__getattribute__(attr)
+    except AttributeError:
+        # If that fails, try to get it from the form fields
+        if hasattr(obj, 'fields') and attr in obj.fields:
+            return obj[attr]
+        # If all else fails, re-raise the original exception
+        raise
